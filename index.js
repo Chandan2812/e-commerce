00253c883ecf42722539843dc0fb6ef3c411d3f2
Chandan2812 <chandanpms@gmail.com>
2024-02-21 21:33:02 +0530

    database connected

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const {Connection} = require("./config/db")
 require("dotenv").config();
 
 const app = express();
@@ -9,6 +10,12 @@ app.use(express.json());
 
 
 app.listen(PORT,async()=>{
+    try {
+        await Connection
+        console.log("Connected to Database");
+    } catch (error) {
+        console.log('failed to connect to DB');
+    }
     console.log(`Server is running @ ${PORT}`);
 })
 
